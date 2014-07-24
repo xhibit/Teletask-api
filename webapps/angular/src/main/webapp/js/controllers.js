51b0f79b2fd270dba388e0955aabfaa40c4a8ac2
Geroen Dierckx <geroen.dierckx@hp.com>
2014-07-24 19:04:48 +0200

    Better config

--- webapps/angular/src/main/webapp/js/controllers.js
+++ webapps/angular/src/main/webapp/js/controllers.js
@@ -3,15 +3,12 @@
 (function () {
     /* Controllers */
     var app = angular.module('homies-controller', []);
-    app.controller('ConfigController', ['$http', function ($http) {
-//        var url = 'http://localhost:7776';
-        var url = 'http://stewie.griffin.lan:7070/teletask';
-
+    app.controller('ConfigController', ['$rootScope', '$http', function ($rootScope, $http) {
         var controller = this;
 
         var config = [];
 
-        $http.get(url + '/api/config').success(function (data, status, headers, config) {
+        $http.get($rootScope.baseUrl + '/config').success(function (data, status, headers, config) {
             controller.config = data;
         });
     }])
