69dd3157e6adf32bab7362aa99597950009e3398
Geroen Dierckx <geroen.dierckx@hp.com>
2014-07-24 20:19:34 +0200

    Added general /component rest call

--- webapps/angular/src/main/webapp/js/app.js
+++ webapps/angular/src/main/webapp/js/app.js
@@ -5,8 +5,8 @@
     var app = angular.module('homies', ['homies-controller']);
 
     app.run(function ($rootScope) {
-        $rootScope.baseUrl = 'http://localhost:7776/api';
-//        $rootScope.baseUrl = 'http://stewie.griffin.lan:7070/teletask/api';
+//        $rootScope.baseUrl = 'http://localhost:7776/api';
+        $rootScope.baseUrl = 'http://stewie.griffin.lan:7070/teletask/api';
     });
 })();
 //    'homies.filters',
