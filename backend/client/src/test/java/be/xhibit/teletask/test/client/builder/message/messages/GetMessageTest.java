8bae76dd860338a8bd02ad850ab9fd39032a8a0b
Geroen Dierckx <geroen.dierckx@hp.com>
2014-11-15 15:40:19 +0100

    Started work on testing

---
+++ backend/client/src/test/java/be/xhibit/teletask/test/client/builder/message/messages/GetMessageTest.java
@@ -0,0 +1,10 @@
+package be.xhibit.teletask.test.client.builder.message.messages;
+
+import org.junit.Test;
+
+public class GetMessageTest {
+    @Test
+    public void simpleGetMessage() {
+
+    }
+}
