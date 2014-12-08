8be294982988a4c682d234f66016fc3c1e0467aa
Geroen Dierckx <geroen.dierckx@hp.com>
2014-12-08 19:57:35 +0100

    Added logging for removing unused token

--- webapps/rest/src/main/java/be/xhibit/teletask/webapp/rest/ota/OneTimeAccessTokenStoreMemoryImpl.java
+++ webapps/rest/src/main/java/be/xhibit/teletask/webapp/rest/ota/OneTimeAccessTokenStoreMemoryImpl.java
@@ -1,6 +1,8 @@
 package be.xhibit.teletask.webapp.rest.ota;
 
 import be.xhibit.teletask.model.spec.Function;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Map;
 import java.util.Map.Entry;
@@ -10,6 +12,11 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
 
 public class OneTimeAccessTokenStoreMemoryImpl implements OneTimeAccessTokenStore {
+    /**
+     * Logger responsible for logging and debugging statements.
+     */
+    private static final Logger LOG = LoggerFactory.getLogger(OneTimeAccessTokenStoreMemoryImpl.class);
+
     private final Map<String, OneTimeAccessTokenSupport> tokens = new ConcurrentHashMap<>();
 
     public OneTimeAccessTokenStoreMemoryImpl() {
@@ -42,7 +49,9 @@ public class OneTimeAccessTokenStoreMemoryImpl implements OneTimeAccessTokenStor
         @Override
         public void run() {
             for (Entry<String, OneTimeAccessTokenSupport> entry : OneTimeAccessTokenStoreMemoryImpl.this.tokens.entrySet()) {
-                if (!entry.getValue().isValid()) {
+                OneTimeAccessTokenSupport token = entry.getValue();
+                if (!token.isValid()) {
+                    LOG.debug("Removing unused token: {}", token);
                     OneTimeAccessTokenStoreMemoryImpl.this.tokens.remove(entry.getKey());
                 }
             }
